0b4e3e57522f96ca571e65c19c300e59d34d1483
OmarRMC <mamani.capcha.omarrodrigo@gmail.com>
2024-12-24 22:04:58 -0400

    Dev Task Hub

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/prueba', function(Request $request){
-    return response()->json($request); 
-}); 
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(
